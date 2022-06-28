d99ecaf4095981a75283763aeacfc092c48a48e1
BrandonSomoza <83843160+BrandonSomoza@users.noreply.github.com>
2022-06-27 22:42:07 -0400

    changes to redeploy-site

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-tmux kill-session
 cd portfolio
 git fetch && git reset origin/main --hard
 python3.8 -m venv python3-virtualenv
 source python3.8-virtualenv/bin/activate
 pip3.8 install -r requirements.txt
-tmux new ./session.sh
+systemctl restart myportfolio
