ce49b8dffd2de6ff98c7339077bb72e299e0d6df
BrandonSomoza <83843160+BrandonSomoza@users.noreply.github.com>
2022-07-08 16:25:14 -0400

    updated redeploy-site.sh script along with small text change to test functuality

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,7 +2,5 @@
 
 cd portfolio
 git fetch && git reset origin/main --hard
-python3.8 -m venv python3-virtualenv
-source python3.8-virtualenv/bin/activate
-pip3.8 install -r requirements.txt
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
